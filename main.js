669e094c060171f526fe5b7c5cbc13ec7e4d3e59
пк <kinrdatvictor07@gmail.com>
2022-08-27 08:42:19 +0300

    upd

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 let database = JSON.parse(localStorage.getItem('database')) || [];
 let activeAccount = {};
+let x;
+
+function setMainPage(name) {
+    x = setInterval(() => {
+        let date = new Date();
+        date.getHours() < 10 ? $('.date__hours').text('0' + date.getHours()) : $('.date__hours').text(date.getHours())
+        date.getMinutes() < 10 ? $('.date__minutes').text('0' + date.getMinutes()) : $('.date__minutes').text(date.getMinutes())
+        $('.date__text').text(date.toDateString());
+        if (date.getHours() >= 0 && date.getHours() < 12) {
+            $('.mainPage__headline').html(`Good morning ☀, <br> ${name}`);
+        } else if (date.getHours() >= 12 && date.getHours() < 18) {
+            $('.mainPage__headline').html(`Good afternoon ☕, <br> ${name}`);
+        } else {
+            $('.mainPage__headline').html(`Good evening 🌙, <br> ${name}`);
+        }
+    }, 1000);
+}
 
 function userCard(id, userName) {
     let balance = 100;
@@ -93,22 +110,22 @@ class UserAccount {
     }
 }
 
-let card1 = userCard(1);
+// let card1 = userCard(1);
 
-console.log(card1.getCardInformation())
-card1.putCredits(50)
-console.log(card1.getCardInformation())
+// console.log(card1.getCardInformation())
+// card1.putCredits(50)
+// console.log(card1.getCardInformation())
 
-let user = new UserAccount('Victor', 'qwerty123');
-console.log(user)
-user.addCard()
-user.addCard()
-user.addCard()
-user.addCard()
-console.log(user)
-console.log(user.getCardById(2))
-user.cards[1].takeCredits(40);
-console.log(user.getCardById(2))
+// let user = new UserAccount('Victor', 'qwerty123');
+// console.log(user)
+// user.addCard()
+// user.addCard()
+// user.addCard()
+// user.addCard()
+// console.log(user)
+// console.log(user.getCardById(2))
+// user.cards[1].takeCredits(40);
+// console.log(user.getCardById(2))
 
 $('#signIn').click(function () {
     $('.page').attr('data-hidden', 'true');
@@ -127,9 +144,12 @@ $('#passwordSignUp').on('input', function () {
 
 $('#registerBtn').click(function () {
     let exist = false;
-    for (let i = 0; i !== database.length; i++) {
-        if (database[i].name = $('#nameSignUp').val()) {
-            exist = true
+    if (database.length > 0) {
+        for (let i = 0; i !== database.length; i++) {
+            if (database[i].name = $('#nameSignUp').val()) {
+                exist = true;
+                console.log(database[i].name)
+            }
         }
     }
     if ($('#passwordSignUp').val().length > 8) {
@@ -147,6 +167,62 @@ $('#registerBtn').click(function () {
             activeAccount = database[database.length - 1];
             $('.page').attr('data-hidden', 'true');
             $('.mainPage').attr('data-hidden', 'false');
+            clearInterval(x)
+            setMainPage(userCandidature.name);
+            $('#nameSignUp').val('');
+            $('#passwordSignUp').val('');
+            $('#loginLogin').val('');
+            $('#loginPassword').val('');
+        }
+    }
+})
+
+$('#loginBtn').click(function () {
+    let exist = false;
+    let user;
+    for (let i = 0; i !== database.length; i++) {
+        if (database[i].name === $('#loginLogin').val()) {
+            exist = true;
+            user = database[i]
+        }
+    }
+    if (exist) {
+        if (user.password === $('#loginPassword').val()) {
+            activeAccount = user;
+            $('.page').attr('data-hidden', 'true');
+            $('.mainPage').attr('data-hidden', 'false');
+            clearInterval(x)
+            setMainPage(user.name);
+        } else {
+            alert('⚠ Incorrect password! Try again')
+        }
+    } else {
+        let confirmWindow = confirm(`⚠ Account with name ${$('#loginLogin').val()} doesn't exist\nTo sign up press ok. To discard - cancel.`)
+        if (confirmWindow) {
+            $('.page').attr('data-hidden', 'true');
+            $('.register').attr('data-hidden', 'false');
+            $('#nameSignUp').val('');
+            $('#passwordSignUp').val('');
+            $('#loginLogin').val('');
+            $('#loginPassword').val('');
+        }
+    }
+})
+
+$('#logOutbtn').click(function () {
+    $('.page').attr('data-hidden', 'true');
+    $('.helloPage').attr('data-hidden', 'false');
+})
+
+$('#getMoneyBtn').click(function(){
+    let cardId = prompt('Input card id of card', 1);
+    console.log(cardId)
+    for (let i = 0; i!== database.length; i++) {
+        if (database[i] === activeAccount) {
+            console.log(database[i].cards[cardId].getCardInformation())
+            let limit = database[i].getCardById(1);
+            let amount = prompt(`Input the amount of credits to get. This card's limit is ${limit.transactionLimit}`, limit.transactionLimit)
+            database[i].cards[cardId].takeCredits(parseInt(amount))
         }
     }
 })
